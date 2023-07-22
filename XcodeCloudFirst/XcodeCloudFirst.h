883df9e9fafa4e37c1ae346480dc2eac8d261414
Chad B <132126477+cbrianb@users.noreply.github.com>
2023-07-22 02:32:03 -0400

    Initial Commit

---
+++ XcodeCloudFirst/XcodeCloudFirst.h
@@ -0,0 +1,18 @@
+//
+//  XcodeCloudFirst.h
+//  XcodeCloudFirst
+//
+//  Created by Chad Benninger on 7/22/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XcodeCloudFirst.
+FOUNDATION_EXPORT double XcodeCloudFirstVersionNumber;
+
+//! Project version string for XcodeCloudFirst.
+FOUNDATION_EXPORT const unsigned char XcodeCloudFirstVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeCloudFirst/PublicHeader.h>
+
+
